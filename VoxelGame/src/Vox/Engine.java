a7ad6dcab2cc7e7abcd750036bde353f307e2f21
Tyle93 <36911253+Tyle93@users.noreply.github.com>
2019-03-11 18:01:59 -0700

    Changed Vertex Ordering

--- VoxelGame/src/Vox/Engine.java
+++ VoxelGame/src/Vox/Engine.java
@@ -25,6 +25,32 @@ public class Engine{
         for(Cube c : objects){
             c.draw();
         }
+        /*
+        glBegin(GL_QUADS);
+        glColor3f(1.0f,0.0f,1.0f, 1);
+
+
+        glVertex3f( 1.0f,-1.0f,-1.0f);
+        glVertex3f(-1.0f,-1.0f,-1.0f);
+        glVertex3f(-1.0f, 1.0f,-1.0f);
+        glVertex3f( 1.0f, 1.0f,-1.0f);
+
+        glColor3f(0.0f,0.0f,1.0f,1);
+
+        glVertex3f( 1.0f,-1.0f,1.0f);
+        glVertex3f(-1.0f,-1.0f,1.0f);
+        glVertex3f(-1.0f, 1.0f,1.0f);
+        glVertex3f( 1.0f, 1.0f,1.0f);
+
+        glColor3f(1.0f,0.0f,0.0f,1);
+
+        glVertex3f( 1.0f,-1.0f,-1.0f);
+        glVertex3f(1.0f,-1.0f,1.0f);
+        glVertex3f(-1.0f, -1.0f,1.0f);
+        glVertex3f( -1.0f, -1.0f,-1.0f);
+
+        glEnd();
+        */
         //glEnd();
 
     }
--- VoxelGame/src/Vox/Main.java
+++ VoxelGame/src/Vox/Main.java
@@ -14,22 +14,22 @@ public class Main {
     public static void initDisplay(){
         try{
             Vertex[] v = {
-                    new Vertex(-1,-1,1),
-                    new Vertex(1,-1,1),
                     new Vertex(1,1,1),
                     new Vertex(-1,1,1),
-                    new Vertex(-1,-1,-1),
-                    new Vertex(1,-1,-1),
+                    new Vertex(1,-1,1),
+                    new Vertex(-1,-1,1),
                     new Vertex(1,1,-1),
-                    new Vertex(-1,1,-1)
+                    new Vertex(-1,1,-1),
+                    new Vertex(1,-1,-1),
+                    new Vertex(-1,-1,-1)
             };
             QuadFace[] q = {
-                    new QuadFace(v[0],v[1],v[2],v[3],new Color(255,0,0)),
-                    new QuadFace(v[0],v[4],v[5],v[1],new Color(0,255,0)),
-                    new QuadFace(v[4],v[5],v[6],v[7],new Color(0,0,255)),
-                    new QuadFace(v[7],v[6],v[2],v[3],new Color(255,0,255)),
-                    new QuadFace(v[0],v[4],v[7],v[3],new Color(0,255,255)),
-                    new QuadFace(v[1],v[5],v[6],v[2],new Color(255,255,0))
+                    new QuadFace(v[0],v[2],v[3],v[1],new Color(255,0,0)),
+                    new QuadFace(v[0],v[4],v[6],v[2],new Color(0,255,0)),
+                    new QuadFace(v[0],v[1],v[5],v[4],new Color(0,0,255)),
+                    new QuadFace(v[4],v[5],v[7],v[6],new Color(255,0,255)),
+                    new QuadFace(v[5],v[1],v[3],v[7],new Color(0,255,255)),
+                    new QuadFace(v[4],v[5],v[1],v[0],new Color(255,255,0))
             };
             new Cube(q);
             Engine.start();
